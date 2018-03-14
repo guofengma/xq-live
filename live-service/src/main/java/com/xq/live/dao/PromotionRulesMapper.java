4094acc8c09eefed02d8308f110c7b1721d8fcd0
lipeng632930871 <632930871@qq.com>
2018-03-14 14:47:34 +0800

    我的订单加入产品描述

--- live-service/src/main/java/com/xq/live/dao/PromotionRulesMapper.java
+++ live-service/src/main/java/com/xq/live/dao/PromotionRulesMapper.java
@@ -1,6 +1,7 @@
 package com.xq.live.dao;
 
 import com.xq.live.model.PromotionRules;
+import com.xq.live.vo.in.ProRuInVo;
 
 public interface PromotionRulesMapper {
     int deleteByPrimaryKey(Long id);
@@ -10,6 +11,8 @@ public interface PromotionRulesMapper {
     int insertSelective(PromotionRules record);
 
     PromotionRules selectByPrimaryKey(Long id);
+
+    PromotionRules selectBySkuIdAndSkuCode(ProRuInVo inVo);
 
     int updateByPrimaryKeySelective(PromotionRules record);
 

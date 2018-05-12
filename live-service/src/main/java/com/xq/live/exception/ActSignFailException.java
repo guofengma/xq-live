a0d7cc64dec3dae172ab34092adcb2b728cd60d2
lipeng632930871 <632930871@qq.com>
2018-05-12 12:07:01 +0800

    bug修改

--- live-service/src/main/java/com/xq/live/exception/ActSignFailException.java
+++ live-service/src/main/java/com/xq/live/exception/ActSignFailException.java
@@ -4,7 +4,7 @@ package com.xq.live.exception;
  * 新平台活动报名失败异常
  * Created by lipeng on 2018/4/28.
  */
-public class ActSignFailException extends BaseException{
+public class ActSignFailException extends RuntimeException{
     public ActSignFailException(String message) {
         super(message);
     }

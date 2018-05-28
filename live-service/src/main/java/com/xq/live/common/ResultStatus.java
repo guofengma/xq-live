586877ef4e1c80fb2c3930251b6563752af259cd
JokeLong <xiehu18327639757@163.com>
2018-05-28 09:09:31 +0800

    商家配置表

--- live-service/src/main/java/com/xq/live/common/ResultStatus.java
+++ live-service/src/main/java/com/xq/live/common/ResultStatus.java
@@ -127,11 +127,11 @@ public enum ResultStatus {
 
     error_many_cashier(200046,"该用户已经存在于shop_cashier表里了!"),
 
-    error_allocation_update(200047,"更新失败，请检查参数"),
+    error_allocation_update(200047,"更新失败，请检查参数 "),
 
-    error_allocation_insert(200048,"插入信息失败，请检查参数"),
+    error_allocation_insert(200048,"插入信息失败，请检查参数 "),
 
-    getError_allocation_selectList(200049,"没有查询到此信息，请检查参数"),
+    getError_allocation_selectList(200049,"没有查询到此信息，请检查参数 "),
 
     error_pic_file(3, "非法图片文件"), error_pic_upload(4, "图片上传失败"), error_record_not_found(5, "没有找到对应的数据"), error_max_page_size(6, "请求记录数超出每次请求最大允许值"), error_create_failed(7, "新增失败"), error_update_failed(8, "修改失败"), error_delete_failed(9, "删除失败"), error_search_failed(10, "查询失败"), error_count_failed(11, "查询数据总数失败"), error_string_to_obj(12, "字符串转java对象失败"), error_invalid_argument(13, "参数不合法"), error_update_not_allowed(14, "更新失败：%s"), error_duplicated_data(15, "数据已存在"), error_unknown_database_operation(16, "未知数据库操作失败，请联系管理员解决"), error_column_unique(17, "字段s%违反唯一约束性条件"), error_file_download(18, "文件下载失败"), error_file_upload(19, "文件上传失败"),
 
